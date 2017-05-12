3aa8925d50e5871cef1138d6ced23f8a2264445e
Marvin Frachet <marvin.frachet@zenika.com>
2017-05-12 04:35:46 +0200

    Refacto and adding example on readme
    Refacto code and example with good ones
    
    Changing image width

--- Example/index.ios.js
+++ Example/index.ios.js
@@ -4,14 +4,26 @@
  * @flow
  */
 
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import {
   AppRegistry,
   StyleSheet,
   Text,
-  View
+  View,
 } from 'react-native';
 import Placeholder from 'rn-placeholder';
+import CustomPlacehoder from './customPlaceholder';
+
+const styles = StyleSheet.create({
+  container: {
+    marginTop: 70,
+    marginLeft: 20,
+    marginRight: 20,
+  },
+  block: {
+    marginTop: 50,
+  },
+});
 
 export default class Example extends Component {
   componentWillMount() {
@@ -20,23 +32,34 @@ export default class Example extends Component {
       isReadyParagraph: false,
       isReadyLine: false,
       isReadyMedia: false,
+      isCustomReady: false,
     });
-
-    setTimeout(() => this.setState({isReadyImageContent: true}), 2000); // Display ImageContent after 2s
-    setTimeout(() => this.setState({isReadyParagraph: true}), 3000); // Display Paragraph after 3s
-    setTimeout(() => this.setState({isReadyLine: true}), 4000); // Display Line after 4s
-    setTimeout(() => this.setState({isReadyMedia: true}), 5000); // Display ImageContent after 5s
+    setTimeout(() => {
+      this.setState({ isReadyImageContent: true });
+    }, 2000); // Display ImageContent after 2s
+    setTimeout(() => this.setState({ isReadyParagraph: true }), 3000); // Display Paragraph after 3s
+    setTimeout(() => this.setState({ isReadyLine: true }), 4000); // Display Line after 4s
+    setTimeout(() => this.setState({ isReadyMedia: true }), 5000); // Display ImageContent after 5s
+    setTimeout(() => this.setState({ isCustomReady: true }), 6000);
   }
 
   render() {
     return (
       <View style={styles.container}>
-        <Placeholder.ImageContent lineNumber={3} onReady={this.state.isReadyImageContent} animate="fade">
+        <Placeholder.ImageContent
+          lineNumber={3}
+          onReady={this.state.isReadyImageContent}
+          animate="fade"
+        >
           <Text>Image content loaded</Text>
         </Placeholder.ImageContent>
 
         <View style={styles.block}>
-          <Placeholder.Paragraph lineNumber={3} onReady={this.state.isReadyParagraph} color="#ff0000">
+          <Placeholder.Paragraph
+            lineNumber={3}
+            onReady={this.state.isReadyParagraph}
+            color="#ff0000"
+          >
             <Text>Paragraph loaded</Text>
           </Placeholder.Paragraph>
         </View>
@@ -52,20 +75,15 @@ export default class Example extends Component {
             <Text>Media loaded</Text>
           </Placeholder.Media>
         </View>
+
+        <View style={styles.block}>
+          <CustomPlacehoder onReady={this.state.isCustomReady} animate="fade" bgColor="#00ff00">
+            <Text>Custom component loaded</Text>
+          </CustomPlacehoder>
+        </View>
       </View>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    marginTop: 20,
-    marginLeft: 20,
-    marginRight: 20,
-  },
-  block: {
-    marginTop: 50,
-  }
-});
 
 AppRegistry.registerComponent('Example', () => Example);
