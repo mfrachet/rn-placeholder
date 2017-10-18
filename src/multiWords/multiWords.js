9e7f168913d2885b8d99a4f1d5d30762e28e9215
mfrachet <marvin.frachet@gmail.com>
2017-10-18 06:38:44 +0200

    Fix shine for MultiWords

--- src/multiWords/multiWords.js
+++ src/multiWords/multiWords.js
@@ -1,19 +1,18 @@
-import React from 'react';
-import { View, StyleSheet } from 'react-native';
-import PropTypes from 'prop-types';
-import Line from './../line/line';
+import React from "react";
+import { View, StyleSheet } from "react-native";
+import PropTypes from "prop-types";
+import Line from "./../line/line";
 
 const styles = StyleSheet.create({
   container: {
-    flexDirection: 'row',
-    flex: 1,
-  },
+    flexDirection: "row"
+  }
 });
 
 function MultiWords({ words, textSize }) {
   const borderStyle = {
     borderRightWidth: textSize,
-    borderRightColor: 'transparent',
+    borderRightColor: "transparent"
   };
 
   const lastIndex = words.length - 1;
@@ -21,7 +20,10 @@ function MultiWords({ words, textSize }) {
   return (
     <View style={styles.container}>
       {words.map((word, index) => (
-        <View key={index} style={[lastIndex !== index && borderStyle, { width: word.width }]}>
+        <View
+          key={index}
+          style={[lastIndex !== index && borderStyle, { width: word.width }]}
+        >
           <Line textSize={textSize} color={word.color} />
         </View>
       ))}
@@ -33,15 +35,15 @@ MultiWords.propTypes = {
   words: PropTypes.arrayOf(
     PropTypes.shape({
       color: PropTypes.string,
-      width: PropTypes.string.isRequired,
-    }),
+      width: PropTypes.string.isRequired
+    })
   ),
-  textSize: PropTypes.number,
+  textSize: PropTypes.number
 };
 
 MultiWords.defaultProps = {
   words: [],
-  textSize: 12,
+  textSize: 12
 };
 
 export default MultiWords;
