0c91e1007d24be8bcb57db27775f2d4f0a7e6236
mfrachet <marvin.frachet@gmail.com>
2017-10-18 07:39:42 +0200

    Upgrade RN & Enzyme

--- setup/test.setup.js
+++ setup/test.setup.js
@@ -1,2 +1,4 @@
-/* eslint-disable */
-require('react-native-mock/mock')
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+Enzyme.configure({ adapter: new Adapter() });
