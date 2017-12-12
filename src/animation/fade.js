cbcc95f19f72dfc9a3a37f5dcfb4b6533297ec8f
mfrachet <marvin.frachet@zenika.com>
2017-12-12 18:15:38 +0100

    use native driver on fade

--- src/animation/fade.js
+++ src/animation/fade.js
@@ -2,22 +2,28 @@ import React from 'react';
 import { Animated } from 'react-native';
 import PropTypes from 'prop-types';
 
+const START_VALUE = 0.5;
+const END_VALUE = 1;
+const DURATION = 500;
+const useNativeDriver = true;
+
 /**
  * Create a repetitive fadein / fadeout animation
  */
 const Fade = ({ children }) => {
-  const START_VALUE = 0.5;
   const animation = new Animated.Value(START_VALUE);
 
   function start() {
     Animated.sequence([
       Animated.timing(animation, {
-        toValue: 1,
-        duration: 500,
+        toValue: END_VALUE,
+        duration: DURATION,
+        useNativeDriver,
       }),
       Animated.timing(animation, {
         toValue: START_VALUE,
-        duration: 500,
+        duration: DURATION,
+        useNativeDriver,
       }),
     ]).start((e) => {
       if (e.finished) {
