9ff1dfb93c982ac7950ac44e993162812510a886
Marvin Frachet <marvin.frachet@bnc.ca>
2019-04-27 16:54:12 -0400

    fixing tests for imagecontent

--- src/imageContent/__tests__/imageContent.test.js
+++ src/imageContent/__tests__/imageContent.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import { ImageContent } from '../imageContent';
+import { Media } from '../../shapes';
 
 describe('ImageContent', () => {
   let props;
@@ -22,17 +23,37 @@ describe('ImageContent', () => {
     };
   });
 
-  it('should match snapshot', () => {
-    expect(getWrapper()).toMatchSnapshot();
+  describe('render', () => {
+    it('should match snapshot', () => {
+      expect(getWrapper()).toMatchSnapshot();
+    });
   });
 
-  it('should match snapshot with right element', () => {
-    props.position = 'right';
-    expect(getWrapper()).toMatchSnapshot();
-  });
+  describe('logic', () => {
+    it('should render a null component on the left side', () => {
+      props.position = null;
+      const instance = getWrapper().instance();
 
-  it('should match snapshot with left element', () => {
-    props.position = 'left';
-    expect(getWrapper()).toMatchSnapshot();
+      expect(instance.renderLeft()).toBe(null);
+    });
+
+    it('should render a Media component on the left side', () => {
+      const instance = getWrapper().instance();
+
+      expect(instance.renderLeft()).toEqual(<Media hasRadius />);
+    });
+
+    it('should render a null component on the right side', () => {
+      const instance = getWrapper().instance();
+
+      expect(instance.renderRight()).toBe(null);
+    });
+
+    it('should render a Media component on the right side', () => {
+      props.position = 'right';
+      const instance = getWrapper().instance();
+
+      expect(instance.renderRight()).toEqual(<Media hasRadius />);
+    });
   });
 });
