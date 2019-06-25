a515138579a69a77acf07e21657a144886b82ecc
Damian Sznajder <damsznajder@gmail.com>
2019-06-26 01:12:15 +0200

    ✨ Add TypeScript

--- babel.config.js
+++ babel.config.js
@@ -1,9 +1,3 @@
-module.exports = (api) => {
-  const preset = api.env('test')
-    ? 'module:metro-react-native-babel-preset'
-    : ['@babel/preset-env', { modules: false }];
-
-  return {
-    presets: ['@babel/preset-react', preset],
-  };
+module.exports = {
+  presets: ['module:metro-react-native-babel-preset'],
 };
