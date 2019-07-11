d115075f6a0eb31539a183fb7dce3e3dacfb2539
Marvin Frachet <marvin.frachet@gmail.com>
2019-07-11 13:01:01 -0400

    remove all structure

--- babel.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = (api) => {
-  const preset = api.env('test')
-    ? 'module:metro-react-native-babel-preset'
-    : ['@babel/preset-env', { modules: false }];
-
-  return {
-    presets: ['@babel/preset-react', preset],
-  };
-};
--- src/shapes/box/box.style.js
+++
@@ -1,8 +0,0 @@
-export default ({
-  height = 50, width = 50, radius = 0, color = '#efefef',
-}) => ({
-  height,
-  width,
-  borderRadius: radius,
-  backgroundColor: color,
-});
