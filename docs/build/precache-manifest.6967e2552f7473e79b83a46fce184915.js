33f08a507c0cbe4ccea06c0b406771be9e87c08a
Marvin Frachet <marvin.frachet@bnc.ca>
2019-08-05 12:22:50 -0400

    fixing homepage

---
+++ docs/build/precache-manifest.6967e2552f7473e79b83a46fce184915.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e903e71f1fbecc142901d6d0df65225a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3ecb5ccb8567b960851b",
+    "url": "/static/js/2.03de0129.chunk.js"
+  },
+  {
+    "revision": "b29758fd06b5d86ee2e9",
+    "url": "/static/js/main.97fefd07.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
