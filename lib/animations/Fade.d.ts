82e7b2b1198a86b756ee812ee0760c877a96cea5
mfrachet <marvin@gatsbyjs.com>
2020-06-09 15:40:58 +0200

    Generate artifacts

--- lib/animations/Fade.d.ts
+++ lib/animations/Fade.d.ts
@@ -1,8 +1,14 @@
 import React from "react";
 import { ViewProps } from "react-native";
-export declare class Fade extends React.Component<ViewProps> {
+export interface IFade extends ViewProps {
+    duration?: number;
+}
+export declare class Fade extends React.Component<IFade> {
+    static defaultProps: {
+        duration: number;
+    };
     private animation;
-    constructor(props: ViewProps);
+    constructor(props: IFade);
     componentDidMount(): void;
     render(): JSX.Element;
     private start;
