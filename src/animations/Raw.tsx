e7141352081b06a95d55a48d9f70e77244aabb60
mfrachet <marvin@gatsbyjs.com>
2020-09-18 14:55:31 +0200

    Moving components to hooks

--- src/animations/Raw.tsx
+++ src/animations/Raw.tsx
@@ -1,7 +1,4 @@
 import React from "react";
 import { ViewProps } from "react-native";
-import { AnimationContext } from "./context";
 
-export const Raw: React.FC<ViewProps> = ({ children, ...props }) => (
-  <AnimationContext.Provider value={null}>{children}</AnimationContext.Provider>
-);
+export const Raw: React.FC<ViewProps> = ({ children }) => <>{children}</>;
