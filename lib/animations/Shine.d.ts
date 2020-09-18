75f352c5b882fcd4b20aac641b514cdce2741b58
mfrachet <marvin@gatsbyjs.com>
2020-09-18 15:37:48 +0200

    Bumping to v3.0.3

--- lib/animations/Shine.d.ts
+++ lib/animations/Shine.d.ts
@@ -2,5 +2,6 @@ import React from "react";
 import { ViewProps } from "react-native";
 export interface ShineProps extends ViewProps {
     duration?: number;
+    reverse: boolean;
 }
 export declare const Shine: React.FC<ShineProps>;
--- lib/animations/ShineOverlay.d.ts
+++ lib/animations/ShineOverlay.d.ts
@@ -1,5 +1,6 @@
 import React from "react";
 export interface ShineOverlayProps {
     duration?: number;
+    reverse?: boolean;
 }
 export declare const ShineOverlay: React.FC<ShineOverlayProps>;
